58da7d06acf9500d7bb71d9bb8f1e00a1419b5e2
พงษ์ชนก สุขารมย์ <golfkab007@yahoo.com>
2023-05-29 22:10:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px'
+    },
+    container:{
+      center:true,
+      padding:'1rem'
+    },
+    extend: {
+      fontFamily:{
+        Poppins:"'Poppins' , sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary' : '#FD3D57',
+        'secondary' : '324324'
+      }
+    },
+  },
+  varitants:{
+    extend:{
+      display:['group-hover'],
+      visibility:['group-hover'],
+    },
+  },
+  plugins: [],
+}
